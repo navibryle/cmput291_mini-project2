862b3f86dee22055ca191cf639eac845a040f304
ivan_penales <naviivan321@gmail.com>
2019-11-15 13:45:22 -0700

    Create_files.py now creates the 4 files requested and works up to spec for all files with 10 test items.

--- Phase_1/Create_files.py
+++ Phase_1/Create_files.py
@@ -3,9 +3,9 @@ import xml.etree.ElementTree as ET
 
 class Create_files:
     def __init__(self,filename):
+        self.__filename = filename
         self.__tree = ET.parse(filename)
         self.__root = self.__tree.getroot()
-        self.__term = 'gas'
         self.__elem = None
     def create_file(self):
         #this function will call the four methods below to create files and will also itreate through the xml doc
@@ -16,34 +16,71 @@ class Create_files:
             self.emails()
             self.dates()
             self.recs()
+    #========HELPER METHODS====================================================================================
+    @staticmethod
+    #
+    def format_text(txt):
+        return txt.replace('&',' ').replace(',',' ').replace('.',' ').replace('<',' ').replace('>',' ')\
+                .replace("'" ,' ').replace('"',' ').replace(':',' ').replace(';',' ').replace('/',' ')\
+                    .replace('?',' ').replace("!",' ').replace('|',' ').replace("\\",' ').replace('(',' ').replace(')','')\
+                        .replace('%',' ').replace('=',' ').replace('$',' ').replace('+',' ').lower()
+    @staticmethod
+    def check_if_empty(txt):
+        if  txt == None: return ''
+        else: return txt
+    @staticmethod
+    def convert_to_xml(txt):
+        if txt == None:return ''
+        else:
+            return txt.replace('&','&amp;').replace('\n','&#10;').replace('<','&lt;').replace('>','&gt;')\
+            .replace("'",'&apos;').replace('"','&quot;')
+    #==========================================================================================================
     def terms(self):
-        #this method will create the term file
-        if self.__elem[4].text == None: subj = ''
-        else:subj = self.__elem[4].text.split()
-        if self.__elem[7].text == None: body = ''
-        else:body = self.__elem[7].text.split()
-        if len(subj) > len(body): max_length = len(subj)
-        else: max_length = len(body)
+        #this method will create terms.txt
+        subj = self.format_text(self.check_if_empty(self.__elem[4].text)).split()
+        body = self.format_text(self.check_if_empty(self.__elem[7].text)).split()
+        max_length = max(len(subj),len(body))
         output = open('terms.txt','a')
-        for item in range(max_length-1):
+        for item in range(max_length):
             if item < len(subj):
-                subj_txt = self.format_text(subj[item])
-                if subj_txt == self.__term and not(len(subj_txt) <= 2):
+                subj_txt = subj[item]
+                if not(len(subj_txt) <= 2):
                     output.write('s-{}:{}\n'.format(subj_txt.lower(),self.__elem[0].text))
-            if item < len(body):
-                body_txt = self.format_text(body[item])
-                if body_txt == self.__term and not(len(body_txt) <= 2):
+            if item < len(body) :
+                body_txt = body[item]
+                if not(len(body_txt) <= 2):
                     output.write('b-{}:{}\n'.format(body_txt.lower(),self.__elem[0].text))
         output.close()
-    @staticmethod
-    def format_text(txt):
-        return txt.replace(',','').replace('.','').replace('&#number;','').replace('&#10;','').replace('&lt','').replace('&gt;','')\
-                .replace('&amp;','').replace('&apos;','').replace('&quot;','').lower()
     def emails(self):
-        pass
+        #this method will create emails.txt
+        #this method will assume that from,to,cc,bcc will only contain emails
+        from_mail = self.check_if_empty(self.__elem[2].text).split()
+        to = self.check_if_empty(self.__elem[3].text).split()
+        cc = self.check_if_empty(self.__elem[5].text).split()
+        bcc = self.check_if_empty(self.__elem[6].text).split()
+        max_length = max(len(from_mail),len(to),len(cc),len(bcc))
+        output = open('emails.txt','a')
+        for item in range(max_length):
+            if len(from_mail) > item and from_mail[item] != '':
+                output.write('from-{}:{}\n'.format(from_mail[item].replace(',','').lower(),self.__elem[0].text))
+            if len(to) > item and to[item] != '':
+                output.write('to-{}:{}\n'.format(to[item].replace(',','').lower(),self.__elem[0].text))
+            if len(cc) > item and cc[item] != '':
+                output.write('cc-{}:{}\n'.format(cc[item].replace(',','').lower(),self.__elem[0].text))
+            if len(bcc) > item and bcc[item] != '':
+                output.write('bcc-{}:{}\n'.format(bcc[item].replace(',','').lower(),self.__elem[0].text))
+        output.close()
     def dates(self):
-        pass
+        #this will create dates.txt
+        output = open('dates.txt','a')
+        output.write('{}:{}\n'.format(self.__elem[1].text,self.__elem[0].text))
+        output.close()
     def recs(self):
-        pass
+        #this will create recs.txt
+        output = open('recs.txt','a')
+        output.write('{}:<mail><row>{}</row><date>{}</date><from>{}</from><to>{}</to><subj>{}</subj><cc>{}</cc><bcc>{}</bcc><body>{}</body></mail>\n'\
+            .format(self.__elem[0].text,self.__elem[0].text,self.__elem[1].text,self.__elem[2].text,self.__elem[3].text,\
+                self.convert_to_xml(self.__elem[4].text),self.convert_to_xml(self.__elem[5].text),self.convert_to_xml(self.__elem[6].text),self.convert_to_xml(self.__elem[7].text)))
+        output.close()
 x = Create_files(sys.argv[1])
 x.create_file()
