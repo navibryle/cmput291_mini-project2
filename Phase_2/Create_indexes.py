7380e88906edd729885e9ac6008014bb7e5aaf97
Ivan Penales <naviivan321@gmail.com>
2019-11-16 12:49:06 -0700

    updated Create_indexes.py

--- Phase_2/Create_indexes.py
+++ Phase_2/Create_indexes.py
@@ -16,10 +16,22 @@ class Create_indexes:
         except:
             sys.exit("Create_indexes.py was not able to open at least one of terms.txt,emails.txt,dates.txt, and recs.txt")
     def sort_files(self):
-        pass
+        os.system('sort -u {}'.format(self.__terms))
+        os.system('sort -u {}'.format(self.__emails))
+        os.system('sort -u {}'.format(self.__emails))
+        os.system('sort -u {}'.format(self.__recs))
     def create_indexes(self):
         #This method will call all the methods below which will create the index files
         self.sort_files()
         self.terms_index()
         self.emails_index()
         self.dates_index()
+        self.recs_index()
+    def terms_index(self):
+        pass
+    def emails_index(self):
+        pass
+    def dates_index(self):
+        pass
+    def recs_index(self):
+        pass
