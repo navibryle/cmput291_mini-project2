542c56e9a8cc7af92dfe54ee3bb38011360272c8
Ivan Penales <naviivan321@gmail.com>
2019-11-17 13:44:11 -0700

    modified Create_indexes.py to be more general

--- Phase_2/Create_indexes.py
+++ Phase_2/Create_indexes.py
@@ -22,25 +22,27 @@ class Create_indexes:
     def create_indexes(self):
         #This method will call all the methods below which will create the index files
         self.sort_files()
-        self.terms_index()
-        self.emails_index()
-        self.dates_index()
-        self.recs_index()
-    def terms_index(self):
-        file_name = 'te.idx'
-        self.__database.open(file_name,None,db.DB_BTREE, db.DB_CREATE)
-        terms_file = open(self.__terms)
-        terms_line = terms_file.readline()
-        while terms_line != '':
-            cut_off = terms_line.index(':')
-            self.__database.put(terms_line[:cut_off].encode('utf-8'),terms_line[cut_off+1:].strip())
-            terms_line = terms_file.readline()
-        terms_file.close()
-    def emails_index(self):
-        pass
-    def dates_index(self):
-        pass
-    def recs_index(self):
-        pass
+        self.tree_index('te.idx',self.__terms)
+        self.tree_index('em.idx',self.__emails)
+        self.tree_index('da.idx',self.__dates)
+        self.hash_index('re.idx',self.__recs)
+    def tree_index(self,output_filename,input_filename):
+        self.__database.open(output_filename,None,db.DB_BTREE, db.DB_CREATE)
+        tree_file = open(input_filename)
+        tree_line = tree_file.readline()
+        while tree_line != '':
+            cut_off = tree_line.index(':')
+            self.__database.put(tree_line[:cut_off].encode('utf-8'),tree_line[cut_off+1:].strip())
+            tree_line = tree_file.readline()
+        tree_file.close()
+    def hash_index(self,output_filename,input_filename):
+        self.__database.open(output_filename,None,db.DB_HASH, db.DB_CREATE)
+        hash_file = open(input_filename)
+        hash_line = hash_file.readline()
+        while hash_line != '':
+            cut_off = hash_line.index(':')
+            self.__database.put(hash_line[:cut_off].encode('utf-8'),hash_line[cut_off+1:].strip())
+            hash_line = hash_file.readline()
+        hash_file.close()
 x = Create_indexes()
 x.create_indexes()
