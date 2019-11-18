24f684bc26acb4bef65334ed81efb05e64b19e5b
Ivan Penales <naviivan321@gmail.com>
2019-11-18 14:41:37 -0700

    Create indexes properly outputting 4 files

--- Phase_2/Create_indexes.py
+++ Phase_2/Create_indexes.py
@@ -13,7 +13,6 @@ class Create_indexes:
         self.__emails = 'emails.txt'
         self.__dates = 'dates.txt'
         self.__recs = 'recs.txt'
-        self.__database = db.DB()
     def sort_files(self):
         os.system('sort -u {}'.format(self.__terms))
         os.system('sort -u {}'.format(self.__emails))
@@ -27,22 +26,27 @@ class Create_indexes:
         self.tree_index('da.idx',self.__dates)
         self.hash_index('re.idx',self.__recs)
     def tree_index(self,output_filename,input_filename):
-        self.__database.open(output_filename,None,db.DB_BTREE, db.DB_CREATE)
+        database = db.DB()
+        database.open(output_filename,None,db.DB_BTREE, db.DB_CREATE)
         tree_file = open(input_filename)
         tree_line = tree_file.readline()
         while tree_line != '':
             cut_off = tree_line.index(':')
-            self.__database.put(tree_line[:cut_off].encode('utf-8'),tree_line[cut_off+1:].strip())
+            database.put(tree_line[:cut_off].encode('utf-8'),tree_line[cut_off+1:].strip())
             tree_line = tree_file.readline()
         tree_file.close()
+        database.close()
+        return 
     def hash_index(self,output_filename,input_filename):
-        self.__database.open(output_filename,None,db.DB_HASH, db.DB_CREATE)
+        database = db.DB()
+        database.open(output_filename,None,db.DB_HASH, db.DB_CREATE)
         hash_file = open(input_filename)
         hash_line = hash_file.readline()
         while hash_line != '':
             cut_off = hash_line.index(':')
-            self.__database.put(hash_line[:cut_off].encode('utf-8'),hash_line[cut_off+1:].strip())
+            database.put(hash_line[:cut_off].encode('utf-8'),hash_line[cut_off+1:].strip())
             hash_line = hash_file.readline()
         hash_file.close()
+        database.close()
 x = Create_indexes()
 x.create_indexes()
--- Phase_2/check_db.py
+++ Phase_2/check_db.py
@@ -1,9 +1,9 @@
 from bsddb3 import db 
-
 database = db.DB()
-database.open('te.idx')
+database.open('re.idx')
 curr = database.cursor()
 iter = curr.first()
 while iter:
     print(iter)
     iter = curr.next()
+database.close()
